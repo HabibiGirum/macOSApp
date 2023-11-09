606dbde607dd170b3b67071729e577c0ea36f4a8
Habtamu <habtamugirum0489@gmail.com>
2023-11-09 09:21:27 +0300

    macOSApp

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+from setuptools import setup
+
+APP = ['script.py']
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'app.icns',
+    'plist': {
+        'CFBundleName': 'Hello World',
+        'CFBundleIdentifier': 'com.example.hello',
+        'CFBundleVersion': '1.0.0',
+        'CFBundleShortVersionString': '1.0.0',
+    }
+}
+
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
